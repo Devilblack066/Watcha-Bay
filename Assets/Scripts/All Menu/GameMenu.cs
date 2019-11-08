2ba9eceac99c1daa1c78b143e707063ef2b78d1d
FairyPuck <julian.hamboyan@gmail.com>
2019-11-08 17:34:16 +0100

    Modification 08/11/11

---
+++ Assets/Scripts/All Menu/GameMenu.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMenu : MonoBehaviour
+{
+    // Arrêt de la partie 
+    public void GameOver()
+    {
+        Debug.Log("Arrêt de la partie");
+    }
+}
---
+++ Assets/Scripts/All Menu/MainMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void Option ()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Quitter ()
+    {
+        Debug.Log("Application Fermée");
+        Application.Quit();
+    }
+}
