debbd1f71c88b6dde0ce7f44c1c8049d46425e31
Devilblack066 <44459633+Devilblack066@users.noreply.github.com>
2019-11-13 09:28:21 +0100

    Début des besoins

---
+++ Assets/Scripts/IA/ANeed.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ANeed : MonoBehaviour
+{
+    public typeOfNeed Type;
+    public string Name;
+    public int Value;
+    public int MaxValue;
+
+    float timer = 0.0f;
+
+
+    ANeed(typeOfNeed ton, string s, int val, int max) { Type = ton; Name = s; Value = val;  MaxValue = max; }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public static ANeed[] generateBasicalNeeds()
+    {
+        return new ANeed[] {new ANeed(typeOfNeed.Happinness,"Happiness",100,100),
+                            new ANeed(typeOfNeed.Hunger,"Hunger",100,100),
+                            new ANeed(typeOfNeed.Thirst,"Thrist",100,100),
+                            new ANeed(typeOfNeed.Entertainment,"Entertainment",100,100),
+                            new ANeed(typeOfNeed.Tiredness,"Tiredness",100,100)};
+    }
+}
+
+public enum typeOfNeed
+{
+    Happinness,
+    Hunger,
+    Thirst,
+    Entertainment,
+    Tiredness
+}
--- Assets/Scripts/IA/Swimmer.cs
+++ Assets/Scripts/IA/Swimmer.cs
@@ -7,13 +7,15 @@ public class Swimmer : MonoBehaviour
 {
     PatrolScript myPatrol;
     NavMeshAgent myAgent;
-
+    ANeed[] myNeeds;
     Vector3 Destination;
     // Start is called before the first frame update
     void Start()
     {
         myPatrol = GetComponent<PatrolScript>();
         myAgent  = GetComponent<NavMeshAgent>();
+        myNeeds = ANeed.generateBasicalNeeds();
+        StartCoroutine(decreaseRessByTime(5.0f));
         Destination = myPatrol.FindAPointOnBeach();
         MoveTo();
     }
@@ -21,15 +23,44 @@ public class Swimmer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Physics.OverlapBox(gameObject.transform.position, transform.localScale / 2);
+        Collider[] colliders = Physics.OverlapBox(gameObject.transform.position, transform.localScale / 2);
         if (Vector3.Distance(this.transform.position,Destination) < 500)
         {
             Destination = myPatrol.FindAPointOnBeach();
             MoveTo();
         }
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            showNeeds();
+        }
     }
     void MoveTo()
     {
         myAgent.SetDestination(Destination);
+    }
+    void showNeeds()
+    {
+        foreach (ANeed need in myNeeds)
+        {
+            Debug.Log("Pour " + need.Name + " a pour valeur " + need.Value + "/" + need.MaxValue);
+        }
+    }
+
+
+    IEnumerator decreaseRessByTime(float delay = 5.0f)
+    {
+        while (true)
+        {
+            for (int i = 0;i<myNeeds.Length;++i)
+            {
+                if (myNeeds[i].Value > 0)
+                {
+                    myNeeds[i].Value -= 1;
+                }
+            }
+            //Debug.Log("?");
+            yield return new WaitForSeconds(delay);
+        }
     }
 }
