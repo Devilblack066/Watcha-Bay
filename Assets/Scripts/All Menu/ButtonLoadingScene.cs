8fbbc61b9c2841e4c48a33e42a3f5b89a8849f19
FairyPuck <julian.hamboyan@gmail.com>
2020-01-31 14:27:33 +0100

    Transition Menu principal/SampleScene OK

--- Assets/Scripts/All Menu/ButtonLoadingScene.cs
+++ Assets/Scripts/All Menu/ButtonLoadingScene.cs
@@ -6,11 +6,13 @@ public class ButtonLoadingScene : MonoBehaviour
 {
    public void LoadGame()
     {
+        Debug.Log("LoadGame");
         MainMenu.Start_game.StartShowLoadingView(LoadingType.Game);
     }
 
     public void LoadMenu()
     {
+        Debug.Log("LoadMenu");
         MainMenu.Start_game.StartShowLoadingView(LoadingType.MainMenu);
     }
 }
