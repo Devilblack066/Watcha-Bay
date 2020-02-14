48efcb96b74cbda647f2a0caf7c3c2e567e68914
Devilblack066 <44459633+Devilblack066@users.noreply.github.com>
2020-02-14 08:49:45 +0100

    Correction des infos des batiments

--- Assets/Scripts/All Menu/ConstructionWindow.cs
+++ Assets/Scripts/All Menu/ConstructionWindow.cs
@@ -7,12 +7,15 @@ public class ConstructionWindow : MonoBehaviour
 {
     public GameObject SelectedBuild = null;
     public BayStats bay;
+    BuildingInfo theInfoWindow;
 
     // Start is called before the first frame update
     void Start()
     {
         this.gameObject.SetActive(false);
         SelectedBuild = null;
+        theInfoWindow = GetComponentInChildren<BuildingInfo>();
+        theInfoWindow.updateInfo();
     }
 
     // Update is called once per frame
@@ -34,13 +37,14 @@ public class ConstructionWindow : MonoBehaviour
         {
             SelectedBuild = null;
             go.GetComponent<Image>().color = Color.white;
+            theInfoWindow.updateInfo();
         }
         else
         {
             SelectedBuild = go;
             SelectedBuild.GetComponent<Image>().color = Color.green;
             SelectedBuild.GetComponent<BuildingIcon>().Prefab.GetComponent<BuildingScript>().CatchBuildingBonus();
-            GetComponentInChildren<BuildingInfo>().updateInfo();
+            theInfoWindow.updateInfo();
         }
     }
 }
